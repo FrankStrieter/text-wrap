f30911e31fc09c365722f502d8563a2ce7dc3893
Frank Strieter <fs@co-it.eu>
2020-05-20 20:51:43 +0200

    feat(text-wrap): wrap a given text to fixed with

---
+++ text-wrap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace text_wrap
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0] ?? "text.txt";
+            int lineSize = args[1] != "" ? int.Parse(args[1]) : 25;
+            TextWrapper wrapper = new TextWrapper();
+            Console.WriteLine(wrapper.Load(fileName).wrap(lineSize));
+        }
+    }
+}
---
+++ text-wrap/TextWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace text_wrap
+{
+    public class TextWrapper
+    {
+        public TextWrapper(){ }
+
+        private string text = "";
+
+        public TextWrapper Load(string fileName)
+        {
+            string currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            text = System.IO.File.ReadAllText(Path.Combine(currentDirectory, fileName));
+            return this;
+        }
+
+        public string wrap(int lineSize)
+        {
+            return Regex.Replace(text, @"(.{1," + lineSize + @"})(\s|$)", "$1\n");
+        }
+    
+    }
+
+}
+
